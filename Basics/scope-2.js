e1c384741a7d35c9800cbc7e45738963da393819
Matthew Sherman <contact@matthewsherman.ca>
2019-05-14 22:15:09 -0400

    Completed Section 3: JavaScript Basics

---
+++ Basics/scope-2.js
@@ -0,0 +1,19 @@
+// Global
+  // Local
+    // Local (name)
+  // Local
+
+///let name = 'Matthew'
+
+if (true) {
+    //let name = 'Mike'
+
+    if (true) {
+        let name = 'Jen'
+        console.log(name)
+    }
+}
+
+if (true) {
+    console.log(name)
+}
---
+++ Basics/strings.js
@@ -0,0 +1,5 @@
+let city = 'Kingsville'
+let country = 'Canada'
+let location = city + ', ' + country
+
+console.log(location)
