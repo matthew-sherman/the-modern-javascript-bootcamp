d4472a18a84680aa44e08f365a30943b547d2b1f
Matthew Sherman <matthew-sherman@outlook.com>
2020-01-25 22:32:48 -0500

    Completed Section 13: Converting To-Do App

--- Todo-app/src/views.js
+++ Todo-app/src/views.js
@@ -1,46 +1,14 @@
-'use strict'
-
-// Fetch existing todos from localStorage
-const getSavedTodos =  () => {
-    const todosJSON = localStorage.getItem('todos')
-
-    try {
-        return todosJSON ? JSON.parse(todosJSON) : []
-    } catch (e) {
-        return []
-    }
-}
-
-// Save todos to localStorage
-const saveTodos = (todos) => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-}
-
-// Remove todo by id
-const removeTodo = (id) => {
-    const todoIndex = todos.findIndex((todo) => todo.id === id)
-
-    if (todoIndex > -1) {
-        todos.splice(todoIndex, 1)
-    }
-}
-
-// Toggle the completed value for a given todo
-const toggleTodo = (id) => {
-    const todo = todos.find((todo) => todo.id === id)
-
-    if (todo) {
-        todo.completed = !todo.completed
-    }
-}
+import { getTodos, toggleTodo, removeTodo } from './todos'
+import { getFilters } from './filters'
 
 // Render application todos based on filters
-const renderTodos = (todos, filters) => {
+const renderTodos = () => {
+    const filters = getFilters()
     const todoEl = document.querySelector('#todos')
-    const filteredTodos = todos.filter((todo) => {
+    const filteredTodos = getTodos().filter((todo) => {
         const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
         const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-        
+
         return searchTextMatch && hideCompletedMatch
     })
 
@@ -64,7 +32,7 @@ const renderTodos = (todos, filters) => {
     // Else, p with class "empty-message" and message "No to-dos to show"
 }
 
-// Get the DOM elements for an individual note
+// Get the DOM elements for an individual todo
 const generateTodoDOM = (todo) => {
     const todoEl = document.createElement('label')
     const containerEl = document.createElement('div')
@@ -78,8 +46,7 @@ const generateTodoDOM = (todo) => {
     containerEl.appendChild(checkbox)
     checkbox.addEventListener('change', () => {
         toggleTodo(todo.id)
-        saveTodos(todos)
-        renderTodos(todos, filters)
+        renderTodos()
     })
 
     // Setup the todo text
@@ -97,8 +64,7 @@ const generateTodoDOM = (todo) => {
     todoEl.appendChild(removeButton)
     removeButton.addEventListener('click', () => {
         removeTodo(todo.id)
-        saveTodos(todos)
-        renderTodos(todos, filters)
+        renderTodos()
     })
 
 
@@ -116,3 +82,5 @@ const generateSummaryDOM = (incompleteTodos) => {
 
     return summary
 }
+
+export { renderTodos, generateTodoDOM, generateSummaryDOM }
